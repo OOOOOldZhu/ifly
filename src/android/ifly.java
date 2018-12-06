1a0aa8499537f5abf19458d8d852111b3e07420e
z <zhuhaiwen@microduino.cc>
2018-12-06 17:12:48 +0800

    init

---
+++ src/android/ifly.java
@@ -0,0 +1,37 @@
+package com.microduino.mDesigner;
+
+import org.apache.cordova.CordovaPlugin;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * ：Created by z on 2018/12/05
+ */
+
+public class ifly extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+        if ("start".equals(action)) {
+            // 获取activity和context --> cordova.getActivity()和cordova.getContext()
+            Toast.makeText(cordova.getContext(), args.getString(0), Toast.LENGTH_SHORT).show();
+
+
+            return true;
+        }
+
+        return false;
+    }
+
+}
